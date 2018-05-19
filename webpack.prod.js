f42aac723a59311b27693333cbf9069c29e53674
avivharuzi <avivharuzi@gmail.com>
2018-05-19 16:15:29 +0300

    update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
